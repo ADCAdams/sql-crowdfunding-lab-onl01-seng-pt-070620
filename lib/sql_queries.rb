816b39da0d3e7a8a814505b206c8b1d9240d03f2
Andy Adams <andrewdcadams@gmail.com>
2020-09-23 01:22:21 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -44,10 +44,7 @@ end
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   "SELECT pledges.category, pledges.amount
   FROM pledges 
-  JOIN pledges 
-  ON users.id = pledges.user_id 
-  GROUP BY users.name
-  ORDER BY SUM(pledges.amount);"
+  WHERE pledges.category =' music'
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
